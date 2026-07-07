e758799d493fec0380e1cd131493d84b7d2d0b56
Eberechukwu Loveth Agbo <agboisabel44@gmail.com>
2026-07-07 05:35:14 +0100

    load sales dataset with pandas

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-print("Hello, RetailIQ")
+import pandas as pd
+
+sales_data = pd.read_csv("data/sales.csv")
+
+print(sales_data)
+
+print("\nNumber of rows:")
+print(len(sales_data))
+
+print("\nTotal price:")
+print(sales_data["Price"].sum())
+sales_data["Revenue"] = sales_data["Quantity"] * sales_data["Price"]
+print(sales_data)
