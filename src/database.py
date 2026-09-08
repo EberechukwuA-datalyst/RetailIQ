082c5faadb25cd8e94cb0c11c9df918ddb76ac2e
Eberechukwu Loveth Agbo <agboisabel44@gmail.com>
2026-09-08 11:44:51 +0100

    Add products data to MySQL pipeline

--- src/database.py
+++ src/database.py
@@ -12,10 +12,13 @@ connection = mysql.connector.connect(
 )
 
 cursor = connection.cursor()
-cursor.execute("TRUNCATE TABLE sales")
-sales_data = pd.read_csv("data/sales.csv")
 
+sales_data = pd.read_csv("data/sales.csv")
+products_data = pd.read_csv("data/products.csv")
+cursor.execute("TRUNCATE TABLE sales")
+cursor.execute("TRUNCATE TABLE products")
 print(sales_data)
+
 insert_query = """
 INSERT INTO sales (Product, Quantity, Price)
 VALUES (%s, %s, %s)
@@ -24,6 +27,15 @@ for index, row in sales_data.iterrows():
     cursor.execute(
         insert_query,
         (row["Product"], row["Quantity"], row["Price"])
+    )
+products_insert_query = """
+INSERT INTO products (Product, Category)
+VALUES (%s, %s)
+"""
+for index, row in products_data.iterrows():
+    cursor.execute(
+        products_insert_query,
+        (row["Product"], row["Category"])
     )
 connection.commit()
 print("Sales data loaded into MySQL successfully!")
